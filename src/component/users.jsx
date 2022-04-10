ded4dd21416738db13eec05b6307b1768ba09a92
journy002 <journy002@naver.com>
2022-04-10 15:26:14 +0900

    study react aixos

---
+++ src/component/users.jsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+function Users() {
+  const [users, setUsers] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        setError(null);
+        setUsers(null);
+        setLoading(true);
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        setUsers(response.data);
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
+    fetchUsers();
+  }, []);
+
+  if (loading) return <div>Loading ...</div>;
+  if (error) return <div>Error..!</div>;
+  if (!users) return null;
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          {user.name}/{user.username}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Users;
